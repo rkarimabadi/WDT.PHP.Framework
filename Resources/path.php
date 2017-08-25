d6ce2a42241f459c86b526904bc57f597ab070d2
sajadsalimzadeh <salimzadehsajad@gmail.com>
2017-08-25 11:31:48 +0430

    Commit

--- Resources/path.php
+++ Resources/path.php
@@ -1,31 +1,31 @@
 <?php
 $root = str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\','/',realpath('').'/'));
-define('DIR_SEP',DIRECTORY_SEPARATOR);
+define('Dir_Sep',DIRECTORY_SEPARATOR);
 define('Root_Http',($root[0] == '/' ? $root : '/'.$root));
 define('Root', realpath(''). DIRECTORY_SEPARATOR);
 
 define('Root_Areas_Http',Root_Http.'Areas/');
-define('Root_Areas',Root.'Areas'.DIR_SEP);
+define('Root_Areas',Root.'Areas'.Dir_Sep);
 define('Root_Assets_Http',Root_Http.'Assets/');
-define('Root_Assets',Root.'Assets'.DIR_SEP);
+define('Root_Assets',Root.'Assets'.Dir_Sep);
 define('Root_Contents_Http',Root_Http.'Contents/');
-define('Root_Contents',Root.'Contents'.DIR_SEP);
+define('Root_Contents',Root.'Contents'.Dir_Sep);
 define('Root_Controllers_Http',Root_Http.'Controllers/');
-define('Root_Controllers',Root.'Controllers'.DIR_SEP);
+define('Root_Controllers',Root.'Controllers'.Dir_Sep);
 define('Root_Fonts_Http',Root_Http.'Fonts/');
-define('Root_Fonts',Root.'Fonts'.DIR_SEP);
+define('Root_Fonts',Root.'Fonts'.Dir_Sep);
 define('Root_Images_Http',Root_Http.'Images/');
-define('Root_Images',Root.'Images'.DIR_SEP);
+define('Root_Images',Root.'Images'.Dir_Sep);
 define('Root_Layouts_Http',Root_Http.'Layouts/');
-define('Root_Layouts',Root.'Layouts'.DIR_SEP);
+define('Root_Layouts',Root.'Layouts'.Dir_Sep);
 define('Root_Models_Http',Root_Http.'Models/');
-define('Root_Models',Root.'Models'.DIR_SEP);
+define('Root_Models',Root.'Models'.Dir_Sep);
 define('Root_Resources_Http',Root_Http.'Resources/');
-define('Root_Resources',Root.'Resources'.DIR_SEP);
+define('Root_Resources',Root.'Resources'.Dir_Sep);
 define('Root_Scripts_Http',Root_Http.'Scripts/');
-define('Root_Scripts',Root.'Scripts'.DIR_SEP);
+define('Root_Scripts',Root.'Scripts'.Dir_Sep);
 define('Root_Views_Http',Root_Http.'Views/');
-define('Root_Views',Root.'Views'.DIR_SEP);
+define('Root_Views',Root.'Views'.Dir_Sep);
 
 $request = $_SERVER['REQUEST_URI'];
 $hasquery = strpos($request, '?');
@@ -65,27 +65,27 @@ define('This_Controller_Http',This_Area_Http.This_Controller.'/');
 define('This_Action',$action);
 define('This_Action_Http',This_Controller_Http.This_Action.'/');
 
-define('This_Root',(This_Area == null ? Root : Root_Areas.This_Area.DIR_SEP));
+define('This_Root',(This_Area == null ? Root : Root_Areas.This_Area.Dir_Sep));
 define('This_Root_Http',(This_Area == null ? Root_Http : Root_Areas_Http.This_Area.'/'));
 define('This_Contents_Http',This_Root_Http.'Contents/');
-define('This_Contents',This_Root.'Contents'.DIR_SEP);
+define('This_Contents',This_Root.'Contents'.Dir_Sep);
 define('This_Controllers_Http',This_Root_Http.'Controllers/');
-define('This_Controllers',This_Root.'Controllers'.DIR_SEP);
+define('This_Controllers',This_Root.'Controllers'.Dir_Sep);
 define('This_Fonts_Http',This_Root_Http.'Fonts/');
-define('This_Fonts',This_Root.'Fonts'.DIR_SEP);
+define('This_Fonts',This_Root.'Fonts'.Dir_Sep);
 define('This_Images_Http',This_Root_Http.'Images/');
-define('This_Images',This_Root.'Images'.DIR_SEP);
+define('This_Images',This_Root.'Images'.Dir_Sep);
 define('This_Layouts_Http',This_Root_Http.'Layouts/');
-define('This_Layouts',This_Root.'Layouts'.DIR_SEP);
+define('This_Layouts',This_Root.'Layouts'.Dir_Sep);
 define('This_Models_Http',This_Root_Http.'Models/');
-define('This_Models',This_Root.'Models'.DIR_SEP);
+define('This_Models',This_Root.'Models'.Dir_Sep);
 define('This_Scripts_Http',This_Root_Http.'Scripts/');
-define('This_Scripts',This_Root.'Scripts'.DIR_SEP);
+define('This_Scripts',This_Root.'Scripts'.Dir_Sep);
 define('This_Views_Http',This_Root_Http.'Views/');
-define('This_Views',This_Root.'Views'.DIR_SEP);
+define('This_Views',This_Root.'Views'.Dir_Sep);
 
 define('This_Folder_Http',This_Views_Http.This_Controller.'/');
-define('This_Folder',This_Views.This_Controller.DIR_SEP);
+define('This_Folder',This_Views.This_Controller.Dir_Sep);
 
 function spl_autoload_register_func($namespace) 
 {
