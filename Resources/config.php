f7c052a13763103f9646f0d4ec0bc2d1116a64c8
sajadsalimzadeh <salimzadehsajad@gmail.com>
2017-08-25 11:33:24 +0430

    Coommit

--- Resources/config.php
+++ Resources/config.php
@@ -4,10 +4,10 @@ date_default_timezone_set("Asia/Tehran");
 if(file_exists(Root.'config.json')) {
     $config = json_decode(file_get_contents(Root.'config.json'));
     foreach($config as $tkey=>$tvalue) {
-        $tkey = strtoupper($tkey);
+        $tkey[0] = strtoupper($tkey[0]);
         foreach($tvalue as $key => $value) {
             eval('$value = \''.str_replace('{{',"'.",str_replace('}}',".'",$value)).'\';');
-            $key = strtoupper($key);
+            $key[0] = strtoupper($key[0]);
             define($tkey.'_'.$key,$value);
         }
     }
