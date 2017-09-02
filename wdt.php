f04002ed653d4abb829620b4d739641376cec8e1
Sajad Salimzadeh <salimzadeh.s@ime.co.ir>
2017-09-02 08:20:14 +0430

    Commit

--- wdt.php
+++ wdt.php
@@ -1,46 +1,18 @@
 <?php
-
 /*--------------[Constants]------------------*/
 const Black = 30,Red = 31,Green = 32,Brown = 33,Blue = 34,Purple = 35,Cyan = 36,LightGray = 37,
 DarkGray = 30,LightRed = 31,LightGreen = 32,Yellow = 33,LightBlue = 34,LightPurple = 35,LightCyan = 36,White = 37;
 
 define('Templates', 'Resources/Templates/');
-define('Github', 'https://github.com/sajadsalimzadeh/');
-define('Repository', 'WDT.PHP.Framework');
-define('Branch', 'master');
+define('COLOR',is_bool(strpos('Windows',php_uname('s'))));
+echo is_bool(strpos('Windows',php_uname('s')));
 define('Root', str_replace('\\', '/', __DIR__.'/'));
 /*--------------[Message]------------------*/
-function error($message)
-{
-    setForeColor(Red);
-    echo 'error: ';
-    resetForeColor();
-    echo $message."\n";
-}
-function success($message)
-{
-    setForeColor(Green);
-    echo 'success: ';
-    resetForeColor();
-    echo $message."\n";
-}
-function warning($message)
-{
-    setForeColor(Yellow);
-    echo 'warning: ';
-    resetForeColor();
-    echo $message."\n";
-}
-
-function resetForeColor()
-{
-    echo "\033[".LightGray."m";
-}
-function setForeColor($colorCode)
-{
-    echo "\033[".$colorCode."m";
-}
-
+function error($message) { setForeColor(Red); echo 'error: '; resetForeColor(); echo $message."\n"; }
+function success($message) { setForeColor(Green); echo 'success: '; resetForeColor(); echo $message."\n"; }
+function warning($message) { setForeColor(Yellow); echo 'warning: '; resetForeColor(); echo $message."\n"; }
+function resetForeColor() {echo (COLOR ? "\033[" : "").LightGray."m";}
+function setForeColor($colorCode) {echo (COLOR ? "\033[" : "").$colorCode."m";}
 /*--------------[IO]------------------*/
 function getFiles($source) {
     $files = scandir($source);
@@ -81,94 +53,120 @@ function createDirectory($path)
 function createFile($path, $content = null, $overwrite = false)
 {
     if ($overwrite || !file_exists($path)) {
-        if (file_exists($path)) {
-            $file = fopen($path, 'w');
-            if (is_bool($file)) {
-                error("can not create file.");
-            } else {
-                if ($content != null) {
-                    fwrite($file, $content);
-                }
-                fclose($file);
-                chmod($file, 0777); 
-                success("$path created.");
-            }
+        $file = fopen($path, 'w');
+        if (is_bool($file)) {
+            error("can not create file.");
         } else {
-            error("Directory $path not exists.");
+            if ($content != null) {
+                fwrite($file, $content);
+            }
+            fclose($file);
+            chmod($path, 0777); 
+            success("$path created.");
         }
     } else {
         warning("$path already exists.");
     }
 }
 /*--------------[Help]------------------*/
-function createHelp($usage, array $arguments = [], array $options = [], $help = null)
+function help_string($str,$left = 3,$length = 25) {
+    $str = str_pad($str,$left + strlen($str),' ',STR_PAD_LEFT);
+    return str_pad($str,$length,' ',STR_PAD_RIGHT);
+}
+function help_usage($usage) {return '  '.$usage."\r\n";}
+function help_option() {}
+function help($usage, array $options = [])
 {
-    $maxLength = 0;
-    foreach ($arguments as $value) {
-        if (strlen($value['name']) > $maxLength) {
-            $maxLength = strlen($value['name']);
-        }
-    }
-    foreach ($options as $value) {
-        if (strlen($value['name']) > $maxLength) {
-            $maxLength = strlen($value['name']);
-        }
-    }
-    setForeColor(Yellow);
-    echo "Usage:\r\n";
     resetForeColor();
-    echo '  '.$usage."\r\n";
-    if (count($arguments) > 0) {
-        setForeColor(Yellow);
-        echo "Arguments:\r\n";
-        foreach ($arguments as $value) {
-            setForeColor(Green);
-            echo "  $value[name]";
-            resetForeColor();
-            echo str_repeat(' ', $maxLength + 3 - strlen($value['name']));
-            echo "$value[text]\r\n";
-        }
-    }
+    echo "Usage:\r\n";
+    if(is_array($usage)) foreach($usage as $value) echo help_usage($value);
+    else echo help_usage($usage);
+    
     if (count($options) > 0) {
-        setForeColor(Yellow);
         echo "\r\nOptions:\r\n";
-        foreach ($options as $value) {
-            setForeColor(Green);
-            echo "  $value[name]";
-            resetForeColor();
-            echo str_repeat(' ', $maxLength + 3 - strlen($value['name']));
-            echo "$value[text]\r\n";
+        foreach ($options as $key => $value) {
+            echo help_string($key);
+            echo "$value\r\n";
         }
-    }
-    if ($help != null) {
-        setForeColor(Yellow);
-        echo "\r\nHelp:\r\n";
-        resetForeColor();
-        echo "  $help\r\n";
     }
     resetForeColor();
 }
-/*--------------[Project]------------------*/
-function project_new()
-{
+/*--------------[WDT]------------------*/
+define('Version','1.0.0');
+
+function wdt_usage() {
+    $options = getopt('hv',array('version','help','new','update','make','map'));
+    if(isset($options['v']) || isset($options['version'])) wdt_version();
+    elseif(isset($options['h']) || isset($options['help'])) wdt_help();
+    elseif(isset($options['new'])) new_usage();
+    elseif(isset($options['update'])) update_usage();
+    elseif(isset($options['make'])) make_usage();
+    elseif(isset($options['map'])) map_usage();
+}
+function wdt_help() {
+    help(
+        'php wdt [new,make,map,install,update] [Arguments] [Options]',
+        array(
+            '-h , --help'=>'',
+            '-v , --version'=>'version',
+        )
+    );
+}
+function wdt_version() { echo Version."\r\n"; }
+/*--------------[New]------------------*/
+function new_usage() {
+    $options = getopt('h',array('help'));
+    if(isset($options['h']) || isset($options['help'])) new_help();
+    else new_project();
+}
+function new_help() {
+    help(
+        'php wdt new [Options]',
+        array(
+            '-h , --help'=>'',
+            '-a , --areas'=>'Areas Folder',
+        )
+    );
+}
+function new_project() {
+    $options = getopt('a',array('areas'));
+
     createDirectory(Root. 'Assets');
     createDirectory(Root. 'Contents');
     createDirectory(Root. 'Controllers');
-    createDirectory(Root. 'Fonts');
-    createDirectory(Root. 'Images');
-    createDirectory(Root. 'Layouts');
     createDirectory(Root. 'Models');
     createDirectory(Root. 'Scripts');
     createDirectory(Root. 'Views');
 
-    createFile(Root. '.htaccess', file_get_contents(Templates.'htaccess.txt'));
-    createFile(Root. 'config.php', file_get_contents(Templates.'config.txt'));
+    createFile(Root. '.htaccess', file_get_contents(Templates.'.htaccess'));
+    createFile(Root. 'config.json', file_get_contents(Templates.'config.json'));
+    createFile(Root. 'bundels.json', file_get_contents(Templates.'config.json'));
     
-    if (in_array('-a', $argv)) {
-        createDirectory(Root. 'Areas');
-    }
+    if (isset($options['a'])) createDirectory(Root. 'Areas');
+    if (isset($options['f'])) createDirectory(Root. 'Fonts');
+    if (isset($options['i'])) createDirectory(Root. 'Images');
+    if (isset($options['l'])) createDirectory(Root. 'Layouts');
 }
-function project_update()
+/*--------------[Update]------------------*/
+define('Github', 'https://github.com/sajadsalimzadeh/');
+define('Repository', 'WDT.PHP.Framework');
+define('Branch', 'master');
+
+function update_usage() {
+    $options = getopt('h::',array('help::'));
+    var_dump($options);
+    if(isset($options['h']) || isset($options['help'])) update_help();
+    else update_project();
+}
+function update_help() {
+    help(
+        'php wdt update [Options]',
+        array(
+            '-h , --help'=>''
+        )
+    );
+}
+function update_project()
 {
     $extract = ".update/";
     $folder = $extract.Repository.'-'.Branch.'/';
@@ -193,16 +191,31 @@ function project_update()
     success('Framework updated.');
 }
 /*--------------[Make]------------------*/
+function make_usage() {
+    if ($second == 'area') {
+        make_area($argv[2]);
+    } elseif ($second == 'model') {
+        if ($countv > 2) {
+            make_model($argv[2], $params);
+        } else {
+            warning('please enter name like this -> make:model [name]');
+        }
+    } elseif ($second == 'controller') {
+        if ($countv > 2) {
+            make_controller($argv[2], $params);
+        } else {
+            warning('please enter name like this -> make:controller [name]');
+        }
+    }
+}
 function make_area($name)
 {
     createDirectory('Areas/'. $name);
     $path = 'Areas/'.$name.'/';
     createDirectory($path. 'Contents');
-    //createFile($path.'Contents/','style.css');
     createDirectory($path. 'Controllers');
     createDirectory($path. 'Models');
     createDirectory($path. 'Scripts');
-    //createFile($path.'Scripts/','script.css');
     createDirectory($path. 'Views');
     createFile($path.'Views/_ViewBegin.php');
     createFile($path.'Views/_ViewEnd.php');
@@ -236,6 +249,22 @@ function make_controller($name, $params)
     createFile($cpath. $name.'.php', $content);
 }
 /*--------------[Map]------------------*/
+function map_usage() {
+    global $argv,$argc;
+    if($argc == 2) 
+    if ($second == 'db') {
+        if ($countv > 2) {
+            if (isset($params['-u']) && isset($params['-p'])) {
+                map_db($argv[2], $params);
+            } else {
+                warning('please enter username and password');
+            }
+        } else {
+            warning('please enter dbname like this -> map:db [name]');
+        }
+    }
+}
+function map_help() {}
 function map_db($dbname, $params)
 {
     $host = (isset($params['-h']) ? $params['-h'] : 'localhost');
@@ -268,79 +297,12 @@ function map_db($dbname, $params)
 }
 
 /*--------------[Program]------------------*/
-
-$countv = count($argv);
-$params = array();
-for ($i = 2; $i < $countv; $i++) {
-    $explode = explode('=', $argv[$i]);
-    if (count($explode) > 1) {
-        $params[$explode[0]] = $explode[1];
-    }
-}
-
-
-if ($countv == 1) {
-    createHelp("",
-    [],
-    []
-    );
-} elseif ($argv[1] == 'help') {
-    if ($argv[2] == 'new') {
-        createHelp("new [Options]",
-        [],
-        [
-            ["name"=>"-a, --areas","text"=>"Create Areas Folder"],
-            ["name"=>"-f, --fonts","text"=>"Create Fonts Folder"],
-            ["name"=>"-l, --layouts","text"=>"Create Layouts Folder"]
-        ]);
-    }
-} else {
-    if ($argv[1] == 'new') {
-        project_new();
-    }
-    if ($argv[1] == 'update') {
-        project_update();
-    }
-
-    $first = $second = '';
-    $action = explode(':', $argv[1]);
-    if (count($action) > 0) {
-        $first = $action[0];
-    }
-    if (count($action) > 1) {
-        $second = $action[1];
-    }
-
-    if ($first == 'make') {
-        if ($second == 'area') {
-            make_area($argv[2]);
-        } elseif ($second == 'model') {
-            if ($countv > 2) {
-                make_model($argv[2], $params);
-            } else {
-                warning('please enter name like this -> make:model [name]');
-            }
-        } elseif ($second == 'controller') {
-            if ($countv > 2) {
-                make_controller($argv[2], $params);
-            } else {
-                warning('please enter name like this -> make:controller [name]');
-            }
-        }
-    } elseif ($first == 'map') {
-        if ($second == 'db') {
-            if ($countv > 2) {
-                if (isset($params['-u']) && isset($params['-p'])) {
-                    map_db($argv[2], $params);
-                } else {
-                    warning('please enter username and password');
-                }
-            } else {
-                warning('please enter dbname like this -> map:db [name]');
-            }
-        }
-    }
-    if (in_array('build', $argv)) {
-        mkdir('');
-    }
+echo php_uname('s');
+if ($argc == 1) wdt_help();
+else {
+    if ($argv[1] == '--new') new_usage();
+    elseif ($argv[1] == '--update') update_usage();
+    elseif ($argv[1] == '--make') make_usage();
+    elseif ($argv[1] == '--map') map_usage();
+    else wdt_usage();
 }
